6a5b09c09afca8d442f5a4eee3007cb6b41548b8
Jheney Vilvok <jheyvilvok@gmail.com>
2025-04-10 13:37:19 -0300

    feat: implement MovieCard component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
-import db from '../db';
-import Movie from '../components/Movie';
-import { MovieProps } from '../types';
+import db from "../db";
+import MovieCard from "../components/MovieCard";
+import { MovieProps } from "../types";
 
 export default async function Home() {
-
-  const movies = db.collection('movies');
+  const movies = db.collection("movies");
   const result = (await movies.find({}, {}).toArray()) as MovieProps[];
 
   return (
-    <div>
-      <h1>Movie List</h1>
-        {result.map((movie) => {
-            console.log("movie", movie);
-            return <Movie key={movie._id} movie={movie} />;
-        })}
+    <div className="relative min-h-screen overflow-hidden">
+      <h1 className="text-3xl font-bold underline">Movie List</h1>
+      <div className="absolute h-full w-full">
+        <div className="flex h-full w-full grid-cols-10">
+          <div className="col-span-4 flex h-full flex-1 flex-col px-5 mb-3">
+            <h1 className="text-3xl font-bold underline">Movies</h1>
+          </div>
+          <div className="col-span-6 flex h-full flex-1 flex-col p-4">
+            <div className="flex w-full gap-6">
+              {result.map((movie) => {
+                return <MovieCard key={movie._id} movie={movie} />;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/MovieCard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { MovieProps } from "../types";
+
+export default function MovieCard({ movie }: { movie: MovieProps }) {
+  return (
+    <motion.div
+      className="relative h-52 min-w-[250px] rounded-2xl shadow-md"
+      layout
+      initial={{ scale: 0.8, opacity: 0 }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+        transition: {
+          duration: 0.4,
+        },
+      }}
+      exit={{ scale: 0.8, opacity: 0 }}
+      transition={{
+        type: "spring",
+        damping: 20,
+        stiffness: 100,
+      }}
+    >
+      <motion.img
+        layoutId={movie.Poster}
+        alt="Transition Image"
+        src={movie.Poster}
+        className="absolute h-full w-full rounded-2xl object-cover brightness-75"
+      />
+      <motion.div className="absolute z-10 flex h-full items-end p-4">
+        <motion.div>
+          <motion.div
+            layout
+            className="mb-2 h-[2px] w-3 rounded-full bg-white"
+          ></motion.div>
+          <motion.p layoutId={movie.Genre} className="text-xs text-gray-300">
+            {movie.Genre}
+          </motion.p>
+          <motion.h1
+            layoutId={movie.Title}
+            className="text-xl leading-6 text-white"
+          >
+            {movie.Title}
+          </motion.h1>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+}
